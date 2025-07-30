139b47fb98072d85e8a3df3c2e3e97de9886147c
IvanCarrera <ivanvilac@gmail.com>
2025-07-30 08:26:14 +0200

    E liminacion de comentarios

--- src/main/java/es/cic25/proy009/repository/ArbolRepository.java
+++ src/main/java/es/cic25/proy009/repository/ArbolRepository.java
@@ -4,5 +4,5 @@ import es.cic25.proy009.model.Arbol;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArbolRepository extends JpaRepository<Arbol, Long> {
-    // ...existing code...
+    
 }
